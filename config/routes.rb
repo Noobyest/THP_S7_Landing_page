fb40670cbe41b60c1ccf8d79cb8a76c43f3842a4
gabisilvano <gabrielle.silvano@gmail.com>
2018-08-15 17:50:43 +0200

    controller et scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'statics_pages/landing_1'
+  get '/', to: 'statics_pages#landing_1'
   get 'statics_pages/landing_2'
   get 'statics_pages/landing_3'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
